08aca5c1e29a8a4483288feaadcd235b23ae5734
TrenTech <TrenTech@gmail.com>
2017-06-05 20:05:13 -0400

    Fix copy command

--- src/main/java/com/gmail/trentech/pjw/commands/CMDCopy.java
+++ src/main/java/com/gmail/trentech/pjw/commands/CMDCopy.java
@@ -11,13 +11,20 @@ import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
 import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.action.TextActions;
 import org.spongepowered.api.text.format.TextColors;
 import org.spongepowered.api.world.storage.WorldProperties;
+
+import com.gmail.trentech.pjc.help.Help;
 
 public class CMDCopy implements CommandExecutor {
 
 	@Override
 	public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
+		if (!args.hasAny("srcWorld")) {
+			Help help = Help.get("world copy").get();
+			throw new CommandException(Text.builder().onClick(TextActions.executeCallback(help.execute())).append(help.getUsageText()).build(), false);
+		}
 		WorldProperties properties = args.<WorldProperties> getOne("srcWorld").get();
 
 		String newWorldName = args.<String> getOne("newWorld").get();
